9b3c686ed392b60f6e0411c2c02983d6222ee23f
nemo0702 <abhisingh7294@gmail.com>
2019-11-12 10:21:37 +0530

    Removing print statement

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -4,19 +4,16 @@ Implement quick sort
 def partition(arr, low, high):
     i = low-1
     pivot = arr[high]
-    print("I : ", i)
     for j in range(low, high):
         if arr[j] <= pivot:
             i = i+1
             arr[i], arr[j] = arr[j], arr[i]
     arr[i+1], arr[high] = arr[high], arr[i+1]
-    print(arr)
     return (i+1)
 
 def quick_sort(arr, low, high):
     if low < high:
         pi = partition(arr, low, high)
-        print("Pi : ", pi)
         quick_sort(arr, low, pi-1)
         quick_sort(arr, pi+1, high)
 
